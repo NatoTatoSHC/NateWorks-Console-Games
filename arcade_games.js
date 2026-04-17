a119e1c0ce802b5faf11890db574c575c19eed0e
Nate <lesternate6@gmail.com>
2026-04-16 23:39:35 -0400

    Add New Game - "Bomb Dodger"

--- arcade_games.js
+++ arcade_games.js
@@ -79,135 +79,220 @@ var hashtagEater = {
 }
 var bombDodger = {
     "play": async function (term, id) {
+        //CONSTS
+        const game = {
+            width: term.cols(),
+            height: term.rows() - 3
+        };
+        const groundHeight = 10;
+        const fps = 60;
+        const characters = {
+            ground: "I",
+            player: "@",
+            bomb: "*",
+            air: " "
+        };
+        const explosionSize = 5;
+        const accelaration = 1;
+
+        //SETUP
+        if (!localStorage.getItem(id+".config")) {
+            localStorage.setItem(id+".config", JSON.stringify({
+                drill: false,
+                startScore: 0
+            }));
+        } else {
+            let locData = JSON.parse(localStorage.getItem(id+".config"));
+            let notContainsAllInfo = (!Object.keys(locData).includes("drill") || !Object.keys(locData).includes("startScore"))
+            if (notContainsAllInfo) {
+                console.log("Missing Something")
+                localStorage.setItem(id+".config", JSON.stringify({
+                    drill: false,
+                    startScore: 0
+                }));
+            }
+        }
+        let locData = JSON.parse(localStorage.getItem(id+".config"));
+        var drill = locData.drill;
+        var score = locData.startScore;
+        var iteration = 0;
+        var player = {
+            x: 0,
+            y: 0,
+            yVelo: 1,
+            xVelo: 0
+        };
+        var ground = generateGround();
+        var bombs = [];
+        var grid = generateGrid();
+
+        //FUNCTION
+        function replaceAt(str, index, replacement) {
+            return str.slice(0, index) + replacement + str.slice(index + replacement.length);
+        }
+        function replaceGround(bomb, i) {
+            ground[bomb.y - (game.height - 1 - groundHeight)] = replaceAt(ground[bomb.y - (game.height - 1 - groundHeight)], bomb.x + i, characters.air);
+        }
+        function exit() {
+            clearInterval(timer);
+            term.off("keydown");
+            setTimeout(() => {term.reset();term.echo("Score: "+score)}, 500);
+        }
+        function generateGrid() {
+            let ret = [];
+            for (let i = 0; i < game.height; i++) {
+                let str = characters.air.repeat(game.width);
+                if (i >= game.height - 1 - groundHeight) {
+                    str = ground[i - (game.height - 1 - groundHeight)];
+                }
+                for (b in bombs) {
+                    if (i == bombs[b].y) {
+                        str = str.slice(0, bombs[b].x) + characters.bomb + str.slice(bombs[b].x + 1);
+                    }
+                }
+                if (i == player.y) {
+                    str = str.slice(0, player.x) + characters.player + str.slice(player.x + 1);
+                }
+                //Score
+                if (i == 0) {
+                    str = str.slice(0, 1) + score + str.slice(1 + score.toString().length)
+                }
+                ret.push(str);
+            }
+            return ret;
+        }
+        function generateGround() {
+            let ret = [];
+            for (let i = 0; i < groundHeight; i++) {
+                ret.push(characters.ground.repeat(game.width));
+            }
+            return ret;
+        }
+
+        //EVENTS
         term.on("keydown", (e) => {
             switch(e.key) {
-                case 'd':
-                    if (player.x < game.width - 1) {
-                        player.xVel = 1;
-                    }
-                    break;
                 case 'a':
-                    if (player.x > 0) {
-                        player.xVel = -1;
-                    }
+                    player.xVelo = -1;
+                    break;
+                case 'd':
+                    player.xVelo = 1;
                     break;
                 case 'w':
-                    if (grid[player.y+1][player.x] == "I") {
-                        player.accel = -3;
+                    if (player.y + 1 >= game.height - 1 - groundHeight && ground[player.y + 1 - (game.height - 1 - groundHeight)][player.x] != characters.air) {
                         player.y -= 1;
+                        player.yVelo = -2;
                     }
                     break;
                 case 'x':
-                    clearInterval(loop);
-                    term.off("keydown");
-                    term.off("keyup");
-                    term.reset();
+                    exit();
                     break;
             }
         });
         term.on("keyup", (e) => {
             switch(e.key) {
-                case 'd':
-                    player.xVel = 0;
-                    break;
                 case 'a':
-                    player.xVel = 0;
+                    player.xVelo = 0;
+                    break;
+                case 'd':
+                    player.xVelo = 0;
                     break;
             }
-        })
-        var game = {
-            width: term.cols(),
-            height: term.rows()
-        }
-        var player = {
-            x: 0,
-            y: 0,
-            accel: 1,
-            xVel: 0
-        };
-        var ground = {
-            height: 10,
-            pos: {
-                x: 0,
-                y: game.height-1-10
-            },
-            grid:[]
-        };
-        var bombs = [{x: 10, y: 0}];
-        ground.grid = generateGround(ground)
-        var grid = [];
-        function generateGround(ground) {
-            let retGrid = [];
-            for (let i = 0; i < ground.height; i++) {
-                let str = "I".repeat(game.width);
-                retGrid.push(str);
-            }
-            return retGrid;
-        }
-        function generateGrid(player, ground, bombs) {
-            let retGrid = [];
-            for (let i = 0; i < game.height; i++) {
-                let str = " ".repeat(game.width);
-                if (ground.pos.y <= i && i <= ground.pos.y + ground.height) {
-                    str = ground.grid[i-ground.pos.y];
-                }
-                if (player.y == i) {
-                    str = str.slice(0, player.x)+"@"+str.slice(player.x+1);
-                }
-                bombs.forEach(bomb => {
-                    if (i == bomb.y) {
-                        str = str.slice(0, bomb.x)+"*"+str.slice(bomb.x+1);
-                    }
-                })
-                retGrid.push(str);
-            }
-            return retGrid;
-        }
-grid = generateGrid(player, ground, bombs);
+        });
 
-        function update() {
-            //Hit Top
-            if (player.y < 0) {
-                player.y = 0;
-                player.accel = 1;
-                grid = generateGrid(player, ground, bombs);
-            }
-
-            //Player Fall
-            if (grid[player.y+1][player.x] == " ") {
-                player.y += player.accel;
-            }
-            if (player.accel < 1) {
-                player.accel += 1;
-            }
-
-            //Player MOve
-            if (((player.x > 0 && player.xVel == -1) || (player.x < game.width - 1 && player.xVel == 1))) {
-                if ((player.x > 0 && player.xVel == -1 && grid[player.y][player.x-1] == " ") || (player.x < game.width - 1 && player.xVel == 1 && grid[player.y][player.x+1] == " "))
-                player.x += player.xVel;
-            }
-
-            //Bomb drop
-            bombs.forEach(bomb => {
-                bomb.y += 1;
-                if (grid[bomb.y+1][bomb.x] == "I") {
-                    ground.grid[bomb.y+1-ground.pos.y] = ground.grid[bomb.y+1-ground.pos.y].slice(0, bomb.x-2) + "     "+ground.grid[bomb.y+1-ground.pos.y].slice(bomb.x+3);
-                    delete bombs[bombs.indexOf(bomb)];
-                }
-            });
-
-            grid = generateGrid(player, ground, bombs);
+        //LOOP
+        function loop() {
+            iteration += 1;
+            update();
             draw();
         }
-        function draw() {
-            term.clear();
-            term.set_command("");
-            term.echo("Press x to exit.")
-            grid.forEach(y => {
-                term.echo(y);
-            });
+        var timer = setInterval(loop, 1000/fps)
+
+        //UPDATE
+        function update() {
+            if ((iteration % 60) === 0) {
+                score += 1;
+            }
+            //Player Movement
+            //Move Right
+            if (player.xVelo > 0 && player.x < game.width - 1) {
+                if (player.y >= game.height - 1 - groundHeight) {
+                    if (ground[player.y - (game.height - 1 - groundHeight)][player.x + 1] == characters.air) {
+                        player.x += player.xVelo;
+                    }
+                } else {
+                    player.x += player.xVelo;
+                }
+            }
+            //Move Left
+            if (player.xVelo < 0 && player.x > 0) {
+                if (player.y >= game.height - 1 - groundHeight) {
+                    if (ground[player.y - (game.height - 1 - groundHeight)][player.x - 1] == characters.air) {
+                        player.x += player.xVelo;
+                    }
+                } else {
+                    player.x += player.xVelo;
+                }
+            }
+            //Gravity
+            if (player.y + 1 >= game.height - 1 - groundHeight) {
+                if (player.y >= game.height - 2) {
+                    exit();
+                } else if (ground[player.y + 1 - (game.height - 1 - groundHeight)][player.x] == characters.air) {
+                    player.y += player.yVelo;
+                }
+            } else {
+                player.y += player.yVelo;
+            }
+            if (player.yVelo < 1) {
+                player.yVelo += accelaration;
+            }
+
+            //Make Bombs
+            if (bombs.length < 10) {
+                bombs.push({
+                    x: Math.round(Math.random() * (game.width - 1)),
+                    y: Math.round(Math.random() * (game.height / 4))
+                });
+            }
+            //Bombs' Gravity & Explosion
+            for (b in bombs) {
+                let bomb = bombs[b];
+                bomb.y += 1;
+                if (bomb.y >= game.height - 1) {
+                    bombs.splice(b, 1);
+                } else if (bomb.y >= (game.height - 1 - groundHeight) && ground[bomb.y - (game.height - 1 - groundHeight)][bomb.x] == characters.ground) {
+                    replaceGround(bomb, 0);
+                    if (player.y == bomb.y - 1 && player.x == bomb.x) {
+                        explode.play();
+                        exit();
+                    }
+                    if (!drill) {
+                        bombs.splice(b, 1);
+                    }
+
+                }
+            }
         }
-        var loop = setInterval(update, 10);
+
+        //DRAW
+        function draw() {
+            grid = generateGrid();
+            term.clear();
+            term.echo("Press x to exit");
+            term.set_command("");
+            for (i in grid) {
+                if (grid[i] && grid[i].length > game.width) {
+                    console.log("Too Long");
+                }
+                term.echo(grid[i]);
+            }
+        }
     }
-        
+}
+var pingloushay = {};
+var amperShoot = {
+    "play": function (term, id) {
+
+    }
 }
